a210206f662355c7f20ef8e02cde7fb2ec337644
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-03-06 21:33:45 +0000

    Improve meta description

--- scripts/build-index.js
+++ scripts/build-index.js
@@ -43,7 +43,7 @@ function template(pages) {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta description="Alpine.js Playground" />
+    <meta description="Alpine.js Playground - Ready to use Alpine.js examples" />
     <title>Alpine.js Playground</title>
     <link
       href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"

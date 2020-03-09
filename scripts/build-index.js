f551d573d0f1e090d687b6fba8d0324c81f3ce76
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-03-09 19:23:28 +0000

    :sparkles: Newsletter signup forms

--- scripts/build-index.js
+++ scripts/build-index.js
@@ -7,6 +7,17 @@ if (require.main === module) {
     process.exit(1);
   });
 }
+function getCommit() {
+  return process.env.REPOSITORY_URL && process.env.COMMIT_REF
+    ? {
+      url: `${process.env.REPOSITORY_URL}/commits/${process.env.COMMIT_REF}`,
+      text: process.env.COMMIT_REF
+    }
+    : {
+      url: '',
+      text: 'develop'
+    }
+}
 const hiddenPages = ["index", "thank-you"];
 async function main() {
   const pagePaths = (await fs.readdir(pagesDir)).filter(
@@ -39,6 +50,7 @@ const renderPagesToJSObj = pages =>
     .join(",\n");
 
 function template(pages) {
+  const commit = getCommit();
   return `<!DOCTYPE html>
 <html lang="en">
   <head>
@@ -64,13 +76,15 @@ function template(pages) {
       class="flex mx-auto flex-col items-center px-8 md:px-32 py-24"
     >
       <h2 class="text-xl font-semibold text-gray-900 mb-8">Alpine.js Playground</h2>
-      <div class="text-xs text-gray-500 italic mb-4">Last Updated: ${new Date().toLocaleString(
-        "en-GB"
-      )}</div>
+      <div class="text-xs text-gray-500 italic mb-4">Last update: <a
+        class="text-blue-500 hover:text-blue-800 hover:underline"
+        href="${commit.url}"
+      >${commit.text}</a>
+      </div>
       <p class="mb-4">${
         pkg.description
       } by <a class="text-blue-500 hover:text-blue-800 hover:underline" href="https://codewithhugo.com/tags/alpinejs">Hugo</a></p>
-      <ul class="list-inside">
+      <ul class="list-inside mb-8">
         <template x-for="page in pages" :key="page.path">
           <li class="list-disc w-full">
             <a
@@ -81,6 +95,36 @@ function template(pages) {
           </li>
         </template>
       </ul>
+
+      <form
+        action="https://buttondown.email/api/emails/embed-subscribe/alpinejs"
+        method="post"
+        target="popupwindow"
+        onsubmit="window.open('https://buttondown.email/alpinejs', 'popupwindow')"
+        class="flex md:w-4/5 md:p-10 flex-col rounded mb-4 embeddable-buttondown-form"
+      >
+        <label
+          class="flex text-gray-700 font-semibold mb-2 text-sm"
+          for="bd-email"
+        >
+          Subscribe to Alpine.js Weekly
+        </label>
+        <p class="leading-relaxed flex text-sm mb-4">A free, once–weekly email roundup of Alpine.js news and articles.</p>
+        <input
+          placeholder="calebporzio@alpinejs.org"
+          class="flex mb-2 bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 appearance-none leading-normal"
+          type="email"
+          name="email"
+          id="bd-email"
+          required="required"
+        />
+        <input type="hidden" value="1" name="embed" />
+        <input
+          class="flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+          value="Subscribe"
+        />
+      </form>
     </div>
     <script>
       function page() {

f16010df20841b6fbb1ea869b530749e6352553a
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-04-06 12:44:24 +0100

    inline-css: add in-memory cache

--- scripts/inline-remote-css/loadRemoteStyleSheet.js
+++ scripts/inline-remote-css/loadRemoteStyleSheet.js
@@ -2,6 +2,27 @@ const fs = require("fs").promises;
 const fetch = require("node-fetch");
 const opts = require("./options");
 
+const inMemory = new Map();
+
+const cache = {
+  async set(key, value) {
+    inMemory.set(key, value);
+    await fs.writeFile(key, value, "utf8");
+  },
+  async get(key, options) {
+    const cachedVersion =
+      inMemory.get(key) ||
+      (await fs
+        .readFile(key, "utf8")
+        // No cache for resource, init the directory, if it doesn't exist
+        .catch(() => fs.mkdir(options.cachePath).catch(() => {})));
+
+    inMemory.set(key, cachedVersion);
+
+    return cachedVersion;
+  },
+};
+
 /**
  * Load & cache (to fs) remote stylesheets
  * @param {string} url - Stylesheet URL to load
@@ -16,10 +37,7 @@ async function loadRemoteStyleSheet(url, options = opts()) {
   )}`;
   // cache enabled by default
   if (!noCache) {
-    const cachedVersion = await fs
-      .readFile(styleSheetCachePath, "utf8")
-      // No cache for resource, init the directory, if it doesn't exist
-      .catch(() => fs.mkdir(cachePath).catch(() => {}));
+    const cachedVersion = await cache.get(styleSheetCachePath, options);
     if (cachedVersion) {
       return cachedVersion;
     }
@@ -27,7 +45,7 @@ async function loadRemoteStyleSheet(url, options = opts()) {
 
   const css = await fetch(url).then((res) => res.text());
   // Cache loaded resource
-  await fs.writeFile(styleSheetCachePath, css, "utf8");
+  await cache.set(styleSheetCachePath, css);
 
   return css;
 }

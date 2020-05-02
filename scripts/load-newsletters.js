43f4ae874d46145932c57f9f767afde41310d31e
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-05-02 16:23:15 +0100

    feat(load-newsletters-and-copy-issue-8-links)

---
+++ scripts/load-newsletters.js
@@ -0,0 +1,23 @@
+const buttondown = require('buttondown');
+const fs = require('fs').promises;
+
+const { BUTTONDOWN_API_KEY } = process.env;
+if (!BUTTONDOWN_API_KEY) {
+  console.error('BUTTONDOWN_API_KEY must be set');
+  process.exit(1);
+}
+
+buttondown.setApiKey(process.env.BUTTONDOWN_API_KEY);
+
+// change this to the latest issue whose data's been synced to this repo
+const LAST_SYNCED_NEWSLETTER_NUMBER = 6;
+
+async function main() {
+  const emails = await buttondown.emails.list();
+
+  const latestEmails = emails.filter(e => e.secondary_id > LAST_SYNCED_NEWSLETTER_NUMBER);
+
+  await fs.writeFile('emails.json', JSON.stringify(latestEmails), 'utf-8');
+}
+
+main();

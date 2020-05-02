f7735ce625c68f8e4a45ab14856b390fda77df25
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-05-02 16:33:59 +0100

    feat(copy-links-from-issue-9)

--- scripts/load-newsletters.js
+++ scripts/load-newsletters.js
@@ -10,14 +10,14 @@ if (!BUTTONDOWN_API_KEY) {
 buttondown.setApiKey(process.env.BUTTONDOWN_API_KEY);
 
 // change this to the latest issue whose data's been synced to this repo
-const LAST_SYNCED_NEWSLETTER_NUMBER = 6;
+const LAST_SYNCED_NEWSLETTER_NUMBER = 9;
 
 async function main() {
   const emails = await buttondown.emails.list();
 
   const latestEmails = emails.filter(e => e.secondary_id > LAST_SYNCED_NEWSLETTER_NUMBER);
 
-  await fs.writeFile('emails.json', JSON.stringify(latestEmails), 'utf-8');
+  await fs.writeFile('emails.json', JSON.stringify(latestEmails, null, 2), 'utf-8');
 }
 
 main();

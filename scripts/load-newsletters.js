23a90bc1b28188f6ef6ad42cc69613332a053019
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-05-08 15:50:04 +0100

    feat(add-handbook-landing-page)

--- scripts/load-newsletters.js
+++ scripts/load-newsletters.js
@@ -1,9 +1,9 @@
-const buttondown = require('buttondown');
-const fs = require('fs').promises;
+const buttondown = require("buttondown");
+const fs = require("fs").promises;
 
 const { BUTTONDOWN_API_KEY } = process.env;
 if (!BUTTONDOWN_API_KEY) {
-  console.error('BUTTONDOWN_API_KEY must be set');
+  console.error("BUTTONDOWN_API_KEY must be set");
   process.exit(1);
 }
 
@@ -15,9 +15,15 @@ const LAST_SYNCED_NEWSLETTER_NUMBER = 9;
 async function main() {
   const emails = await buttondown.emails.list();
 
-  const latestEmails = emails.filter(e => e.secondary_id > LAST_SYNCED_NEWSLETTER_NUMBER);
+  const latestEmails = emails.filter(
+    (e) => e.secondary_id > LAST_SYNCED_NEWSLETTER_NUMBER
+  );
 
-  await fs.writeFile('emails.json', JSON.stringify(latestEmails, null, 2), 'utf-8');
+  await fs.writeFile(
+    "emails.json",
+    JSON.stringify(latestEmails, null, 2),
+    "utf-8"
+  );
 }
 
 main();
